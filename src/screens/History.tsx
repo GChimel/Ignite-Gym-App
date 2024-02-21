f9a1aef9969df31fa73fb12daefdfebeb9d1eff2
GChimel <gustavochimel12@gmail.com>
2024-02-21 20:08:12 -0300

    historyCard & ScreenHeader component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,17 @@
-import { Center, Text } from 'native-base';
+import { VStack } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
   return(
-    <Center flex={1}>
-      <Text color={'white'}>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Histórico de Exercícios'/>
+
+      <HistoryCard/>
+      <HistoryCard/>
+      <HistoryCard/>
+
+    </VStack>
   );
 }
